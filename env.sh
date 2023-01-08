3332752fa148f00070a9ab074121a837db74a7e6
北須賀輝明 <kitasuka@kitasuka-ma-m2.local>
2023-01-08 18:17:36 +0900

    環境変数を書くenv.shの雛形を追加

---
+++ env.sh
@@ -0,0 +1,8 @@
+export SLACK_BOT_TOKEN=xoxb-<your-bot-token>
+export SLACK_SETTING_FILENAME=setting.json
+
+# for Socket mode
+export SLACK_APP_TOKEN=xapp-<your-app-token>
+
+# log level
+SLACK_BOLT_LOG_LEVEL=info
